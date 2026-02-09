69659f7d36301b0c0c974ebb60fcfb366898cfcf
Khaled Hosny <khaled@aliftype.com>
2026-02-09 12:17:09 +0200

    Basic compilation

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,121 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+use std::path::Path;
+
+use fea_rs::{
+    compile::{validate, CompilationCtx, NopFeatureProvider, NopVariationInfo},
+    parse::{parse_root, SourceLoadError},
+    DiagnosticSet, GlyphMap,
+};
+
+const MAX_DIAGNOSTICS: usize = 100;
+
+#[wasm_bindgen(getter_with_clone)]
+#[derive(Clone)]
+pub struct InsertMarker {
+    pub tag: String,
+    #[wasm_bindgen(js_name = "lookupId")]
+    pub lookup_id: usize,
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, build-shaper-font!");
+#[wasm_bindgen(getter_with_clone)]
+pub struct CompilationResult {
+    #[wasm_bindgen(js_name = "fontData")]
+    pub font_data: Option<Vec<u8>>,
+    #[wasm_bindgen(js_name = "insertMarkers")]
+    pub insert_markers: Option<Vec<InsertMarker>>,
+    pub messages: String,
+}
+
+#[wasm_bindgen(js_name = buildShaperFont)]
+pub fn build_shaper_font(
+    #[wasm_bindgen(js_name = "unitsPerEm")] units_per_em: u16,
+    #[wasm_bindgen(js_name = "glyphOrder")] glyph_order: Vec<String>,
+    #[wasm_bindgen(js_name = "featureSource")] feature_source: String,
+    _axes: JsValue,
+) -> Result<CompilationResult, JsError> {
+    utils::set_panic_hook();
+
+    let glyph_map: GlyphMap = glyph_order.iter().map(|s| s.as_str()).collect();
+
+    let mut messages = Vec::new();
+
+    const SRC_NAME: &str = "features.fea";
+    let (tree, diagnostics) = parse_root(
+        SRC_NAME.into(),
+        Some(&glyph_map),
+        Box::new(move |s: &Path| {
+            if s == Path::new(SRC_NAME) {
+                Ok(feature_source.clone().into())
+            } else {
+                Err(SourceLoadError::new(
+                    s.to_path_buf(),
+                    "parse_string cannot handle imports",
+                ))
+            }
+        }),
+    )?;
+
+    if !diagnostics.is_empty() {
+        messages.push(diagnostics.display().to_string());
+        if diagnostics.has_errors() {
+            return Err(JsError::new(&messages.join("\n")));
+        }
+    }
+
+    let diagnostics = validate(&tree, &glyph_map, None::<&NopVariationInfo>);
+    if !diagnostics.is_empty() {
+        messages.push(diagnostics.display().to_string());
+        if diagnostics.has_errors() {
+            return Err(JsError::new(&messages.join("\n")));
+        }
+    }
+
+    let mut ctx = CompilationCtx::new(
+        &glyph_map,
+        tree.source_map(),
+        None::<&NopVariationInfo>,
+        None::<&NopFeatureProvider>,
+        Default::default(),
+    );
+    ctx.compile(&tree.typed_root());
+
+    let mut insert_markers: Vec<_> = ctx
+        .insert_markers
+        .iter()
+        .map(|(tag, point)| InsertMarker {
+            tag: tag.to_string(),
+            lookup_id: point.lookup_id.to_raw(),
+        })
+        .collect();
+    insert_markers.sort_by(|a, b| a.tag.cmp(&b.tag));
+
+    match ctx.build() {
+        Ok((mut compilation, warnings)) => {
+            if !warnings.is_empty() {
+                let diagnostics = DiagnosticSet::new(warnings, &tree, MAX_DIAGNOSTICS);
+                messages.push(diagnostics.display().to_string());
+            }
+
+            let mut head_table = compilation.head.take().unwrap_or_default();
+            head_table.units_per_em = units_per_em;
+            compilation.head = Some(head_table);
+
+            let mut builder = compilation.to_font_builder()?;
+            let font_data = builder.build();
+
+            Ok(CompilationResult {
+                font_data: Some(font_data),
+                insert_markers: Some(insert_markers),
+                messages: messages.join("\n"),
+            })
+        }
+        Err(errors) => {
+            let diagnostics = DiagnosticSet::new(errors, &tree, MAX_DIAGNOSTICS);
+            messages.push(diagnostics.display().to_string());
+
+            Err(JsError::new(&messages.join("\n")))
+        }
+    }
 }
--- tests/web.rs
+++ tests/web.rs
@@ -3,11 +3,49 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+use wasm_bindgen::prelude::*;
 use wasm_bindgen_test::*;
+
+use build_shaper_font::build_shaper_font;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn test_build_shaper_font() {
+    let units_per_em = 1000;
+    let glyph_order = vec!["A".to_string(), "V".to_string(), "A.alt".to_string()];
+
+    let feature_source = "
+languagesystem DFLT dflt;
+
+feature kern {
+    pos A V -50;
+    #pos A A.alt (wght=400:-50 wght=900:50 wght=100:0);
+    # Automatic Code
+} kern;
+
+feature ss01 {
+    featureNames {
+        name \"Small Caps\";
+    };
+    sub A by A.alt;
+} ss01;
+
+feature aalt {
+    feature liga;
+    feature ss01;
+} aalt;
+";
+
+    let result = build_shaper_font(
+        units_per_em,
+        glyph_order,
+        feature_source.to_string(),
+        JsValue::NULL,
+    );
+    assert!(result.is_ok());
+    let result = result.unwrap();
+    assert!(result.font_data.is_some());
+    assert!(result.insert_markers.is_some());
+    assert!(!result.messages.is_empty());
 }
