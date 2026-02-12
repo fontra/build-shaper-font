92368c051f126a8947a3e97e63dcee9340cc188f
Khaled Hosny <khaled@aliftype.com>
2026-02-12 06:45:01 +0200

    Test shaping built font with HarfBuzz

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import { buildShaperFont } from '../pkg/build_shaper_font.js';
+import harfbuzz from "harfbuzzjs";
 
 describe('buildShaperFont', function () {
   it('Build font with feature data', function () {
@@ -66,5 +67,53 @@ feature mkmk {
     expect([insertMarkers[0].tag, insertMarkers[0].lookupId]).to.deep.equal(['kern', 1]);
     expect([insertMarkers[1].tag, insertMarkers[1].lookupId]).to.deep.equal(['mark', 1]);
     expect(messages).to.equal('');
+  });
+
+  it('Build font and shape with HarfBuzz', async function () {
+    const unitsPerEm = 2000;
+    const glyphOrder = ['.notdef', 'A', 'V'];
+    const featureSource = `
+languagesystem DFLT dflt;
+
+feature kern {
+    pos A V -50;
+} kern;
+`;
+    const { fontData } = buildShaperFont(unitsPerEm, glyphOrder, featureSource);
+
+    let hb = await harfbuzz;
+
+    const blob = hb.createBlob(fontData);
+    const face = hb.createFace(blob);
+    const font = hb.createFont(face);
+
+    let fontFuncs = hb.createFontFuncs();
+    fontFuncs.setNominalGlyphFunc((_, codepoint) => {
+      const ch = String.fromCodePoint(codepoint);
+      if (glyphOrder.includes(ch)) {
+        return glyphOrder.indexOf(ch);
+      }
+      return 0;
+    });
+
+    fontFuncs.setGlyphHAdvanceFunc(() => {
+      return 100;
+    });
+
+    font.setFuncs(fontFuncs);
+
+    const buffer = hb.createBuffer();
+    buffer.addText('AV');
+    buffer.guessSegmentProperties();
+    hb.shape(font, buffer);
+
+    const infos = buffer.getGlyphInfos();
+    const positions = buffer.getGlyphPositions();
+    expect(infos.length).to.equal(2);
+    expect(positions.length).to.equal(2);
+    expect(infos[0].codepoint).to.equal(1);
+    expect(infos[1].codepoint).to.equal(2);
+    expect(positions[0].x_advance).to.equal(50);
+    expect(positions[1].x_advance).to.equal(100);
   });
 });
